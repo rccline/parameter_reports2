adcc6da2c69ee0b50f5e92a0291b30e80c2d19fb
Robert <rccline@fastmail.fm>
2022-02-21 16:18:05 -0700

    Revise Table-Params.Rmd and iterate.R

--- script/iterate.R
+++ script/iterate.R
@@ -5,35 +5,35 @@ library(here)
 # Get dataset from openintro library
 nba <- openintro::nba_players_19  
 
+### Check to see if each runs  
 # input = here("doc-params", "Table-Params.Rmd")
 
 # render(input = "E:/Physalia-MostOutOf-R/doc-params/Table-Params.Rmd")
 
-render(input = here("doc-params", "Table-Params.Rmd"))
+# render(input = here("doc-params", "Table-Params.Rmd"))
 
-getwd()
+
+# create iterative reports ------------------------------------------------ 
+## If you have run the code prevously, first, clear the output directory 
+## before running code again.  
 
 team <- unique(nba$team)
-teaml <- list(team)
-class(teaml)
-teaml# state <- bad_drivers %>%
-#   pull(state)
 
 output_dir <- here("output")
 
-rm(reports)
 reports <- tibble(
-  input = here("doc-params", "Table-Params.Rmd"),
-  # output_file = stringr::str_c("../output/", team, "-.html"),
+  input = here("docs", "Table-Params.Rmd"), 
+  # output_file = stringr::str_c("../output/", team, "-.html"), 
+  # or, use here()  for output file created in line 17 above 
   output_file = stringr::str_c(output_dir,"/", team, "-.html"),
-  params = map(nba, ~list(team = .))
-  # params = list(teams=teaml)
-)
+  params = map(team, ~list(team = .))
+ )
 
-
+## Check input, output_file and params  
 head(reports) 
 
 reports %>% 
-  pwalk(rmarkdown::render) # , input= "./output/report.Rmd")
+  pwalk(rmarkdown::render) 
 
-# pwalk and map are from purrr package
+# pwalk and map are from purrr package  
+
